eccd7c629044da6f3f6ad7992914f049d8386690
dominik <dominik@dell>
2019-02-04 14:42:20 +0100

    obiekt statek

---
+++ src/com/company/Ship.java
@@ -0,0 +1,36 @@
+public class Ships 
+{
+	private int mastsQuantity;
+	private char ship[][];
+	private String owner; 
+	
+		public int getMastsQuantity()
+		{
+			return(mastsQuantity);
+		}
+		//TODO
+//		public char[][] getShip()
+//		{	
+//			for(i=1;i=mastsQuantity;i++)
+//			{
+//				ships[][]='X';
+//			}
+//			return(ship);
+//		}
+		public String getOwner()
+		{
+			return(owner);
+		}
+			public void setMastsQuantity(int mastsQuantity)
+			{
+				this.mastsQuantity = mastsQuantity;
+			}
+			public void setShip(char [][]ship) 
+			{
+				this.ship = ship;
+			}
+			public void setOwner(String owner)
+			{
+				this.owner = owner;
+			}
+}
